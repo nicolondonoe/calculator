a5d3bf986f5323c2f0ac038a5a0d322d822a8873
nicolondonoe <nicolondonoe@gmail.com>
2026-07-23 20:19:24 -0500

    Define basic functions and add elements to HTML

---
+++ script.js
@@ -0,0 +1,34 @@
+function add(a, b) {
+  return a+b;
+}
+
+function substract(a, b) {
+  return a-b;
+}
+
+function multiply(a, b) {
+  return a*b;
+}
+
+function divide(a, b) {
+  if (b === 0) {
+    return undefined;
+  }
+  return a/b;
+}
+
+function operate (operator, a, b) {
+  if (operator === '+') {
+    return add(a, b);
+  }
+  else if (operator === '-') {
+    return substract(a, b);
+  }
+  else if (operator === '*') {
+    return multiply(a, b);
+  }
+  else if (operator === '/') {
+    return divide(a, b);
+  }
+}
+
